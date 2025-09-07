1aa4ba41187a647fda40d8ac74cb4b43100e1016
Matheus Pacheco <m4theusdev@gmail.com>
2025-09-07 17:52:04 -0300

    feat: add database connection

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+import "dotenv/config";
 import express from "express";
-import routes from "./routes/index.js"
+import sequelize from "./src/db/index.js";
+import routes from "./src/routes/index.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
 app.use("/", routes);
 
 app.listen(PORT, () =>  {
   console.log(`Servidor rodando em http://localhost:${PORT}`)
-})
+});
---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import { Sequelize } from "sequelize";
+
+const db = {
+  name: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD
+}
+
+const sequelize = new Sequelize(db.name, db.user, db.password, {
+  dialect: "mysql",
+  host: db.host
+})
+
+export default sequelize
