8a080c6eb9a786b4eaa487cfe5d602d458fd4785
Matheus Pacheco <m4theusdev@gmail.com>
2025-09-17 08:24:59 -0300

    feat: add Joi to handle body validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,25 @@
 import User from "../db/models/user.js";
 import bcrypt from "bcrypt";
+import Joi from "joi";
+
+const userSchema = Joi.object({
+  name: Joi.string().max(100).allow(null, ""),
+  username: Joi.string().alphanum().min(3).max(30).required(),
+  email: Joi.string().email({ minDomainSegments: 2 }).required(),
+  password: Joi.string().min(8).max(255).required()
+});
 
 async function register(req, res) {
   try {
-    const { name, username, email, password} = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const { name, username, email, password } = req.body;
+    const validationResult = userSchema.validate({ name, username, email, password });
 
+    if (validationResult.error) {
+      console.error("Validation error:", validationResult.error.message)
+      return res.status(400).json({ error: validationResult.error.message })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({ name, username, email, password: hashedPassword });
     return res.status(201).json(user);
   } catch (err) {
@@ -16,7 +30,11 @@ async function register(req, res) {
 
 async function getAll(_, res) {
   try {
-    const users = await User.findAll();
+    const users = await User.findAll({
+      attributes: {
+        exclude: ["id", "password"]
+      }
+    });
     return res.json(users);
   } catch (err) {
     console.error(err);
@@ -44,6 +62,13 @@ async function update(req, res) {
   try {
     const { id } = req.params;
     const { name, username } = req.body;
+    const userPartialSchema = Joi.object({ name: userSchema.extract("name"), username: userSchema.extract("username") })
+    const validationResult = userPartialSchema.validate({ name, username })
+
+    if (validationResult.error) {
+      console.error("Validation error: ", validationResult.error.message)
+      return res.status(400).json({ error: validationResult.error.message })
+    }
 
     const user = await User.findByPk(id);
     if (!user) {
